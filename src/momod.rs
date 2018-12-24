3fc04436c44168677ee9627866cdeafb0c9699aa
Ratchaphon Manosuthi <themelo64@gmail.com>
2018-12-24 21:33:50 +0700

    Documentation and struct renames

--- src/momod.rs
+++ src/momod.rs
@@ -2,7 +2,7 @@ extern crate chrono;
 use momod::chrono::prelude::*;
 use std::path::PathBuf;
 
-pub struct momod {
+pub struct Mod {
     label: String,
     load_order: i64,
     nexus_id: i64,
@@ -10,46 +10,58 @@ pub struct momod {
     fav: bool,
     last_updated: DateTime<Local>
 }
-impl momod {
-    // accessors
+impl Mod {
+    /// Gets the label of the mod
     pub fn get_label(&self) -> &String {
         return &self.label;
     }
+    /// Sets the label of the mod
     pub fn set_label(&mut self, input: String) -> () {
         self.label = input;
     }
+    /// Gets the load order of the mod
     pub fn get_load_order(&self) -> i64 {
         return self.load_order;
     }
+    /// Sets the load order of the mod
     pub fn set_load_order(&mut self, input: i64) -> () {
         self.load_order = input;
     }
+    /// Gets the Nexus ID of the mod
     pub fn get_nexus_id(&self) -> i64 {
         return self.nexus_id;
     }
+    /// Sets the Nexus ID of the mod
     pub fn set_nexus_id(&mut self, input: i64) -> () {
         self.nexus_id = input;
     }
+    /// Gets the directory of the mod
     pub fn get_dir(&self) -> &PathBuf {
         return &self.dir;
     }
+    /// Sets the directory of the mod
     pub fn set_dir(&mut self, input: PathBuf) -> () {
         self.dir = input;
     }
+    /// Gets the favourite status of the mod
     pub fn get_fav(&self) -> bool {
         return self.fav;
     }
+    /// Sets the favourite status of the mod
     pub fn set_fav(&mut self, input: bool) {
         self.fav = input;
     }
+    /// Gets the last updated time of the mod
     pub fn get_last_updated(&self) -> &DateTime<Local> {
         return &self.last_updated;
     }
+    /// Sets the last updated time of the mod to when it's called
     pub fn update(&mut self) -> () {
         self.last_updated = Local::now();
     }
-    pub fn new() -> momod {
-    momod {
+    /// Creates a new Mod
+    pub fn new() -> Mod {
+    Mod {
         label: String::from(""),
         load_order: -1,
         nexus_id: -1,
@@ -59,7 +71,7 @@ impl momod {
     }
 }
 }
-impl std::fmt::Display for momod {
+impl std::fmt::Display for Mod {
     fn fmt(&self, _: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         println!("{}", format!("{}{}", "#", &self.load_order));
         println!("{}", format!("{}{}", "Label: ", &self.label));
