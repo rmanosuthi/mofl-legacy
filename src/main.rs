1e9cd92e543f33d3745bda4a695d411d1c944e65
Ratchaphon Manosuthi <themelo64@gmail.com>
2018-12-27 23:39:45 +0700

    Switch to Application

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate gtk;
+extern crate gio;
 mod momod;
 mod mogame;
 mod moenv;
@@ -6,11 +7,19 @@ mod moui;
 use std::thread::sleep;
 use std::time::Duration;
 use std::path::PathBuf;
+use gio::prelude::*;
+use std::env::args;
+use gtk::prelude::*;
 
 fn main() {
-    let mut ui = moui::UI::new();
-    let mut skyrim = mogame::Game::new(ui.env.get_base_path());
-    ui.env.add_game(skyrim);
-    ui.show();
-    gtk::main();
+        let application = gtk::Application::new("net.mpipo.mofl",
+                                                gio::ApplicationFlags::empty())
+                                           .expect("Initialization failed...");
+
+        application.connect_startup(move |app| {
+            moui::UI::build_ui(app);
+        });
+        application.connect_activate(|_| {});
+
+        application.run(&args().collect::<Vec<_>>());
 }
--- src/moui.rs
+++ src/moui.rs
@@ -1,14 +1,17 @@
-extern crate gtk;
+use gio;
+use gtk;
 use gtk::prelude::*;
-use gtk::{Button, Window, WindowType};
+use gio::prelude::*;
+use gtk::{ApplicationWindow, Builder, Button, TreeStore, Window, WindowType};
 use moenv::Environment;
 pub struct UI {
     window: gtk::Window,
     pub env: Environment,
+    store_mod_categories: TreeStore,
 }
 impl UI {
     /// Creates a new UI instance
-    pub fn new() -> UI {
+    /*pub fn new() -> UI {
         if gtk::init().is_err() {
             println!("Failed to initialize gtk");
             panic!();
@@ -25,8 +28,9 @@ impl UI {
         UI {
             window: window,
             env: Environment::new(),
+            store_mod_categories: builder.get_object::<TreeStore>("treestore-mod-categories").unwrap()
         }
-    }
+    }*/
     /// Displays the window
     pub fn show(&self) -> () {
         self.window.show_all();
@@ -34,5 +38,19 @@ impl UI {
     /// Hides the window
     pub fn hide(&self) -> () {
         self.window.hide();
+    }
+    pub fn build_ui(application: &gtk::Application) {
+        let glade_src = include_str!("window.glade");
+        let builder = Builder::new_from_string(glade_src);
+
+        let window: ApplicationWindow = builder
+            .get_object("mowindow")
+            .expect("Couldn't get mowindow");
+        window.set_application(application);
+        window.connect_delete_event(move |win, _| {
+            win.destroy();
+            Inhibit(false)
+        });
+        window.show_all();
     }
 }
