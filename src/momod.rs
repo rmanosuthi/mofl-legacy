3ab36da7c7bf61cec29ecac0992420c63c569a97
Ratchaphon Manosuthi <themelo64@gmail.com>
2018-12-28 21:01:02 +0700

    Support for parsing from mod list ListStore

--- src/momod.rs
+++ src/momod.rs
@@ -1,17 +1,20 @@
 extern crate chrono;
+use gtk::prelude::*;
+use gtk::ListStore;
 use momod::chrono::prelude::*;
 use std::path::PathBuf;
 
 pub struct Mod {
-    label: String,
+    enabled: bool,
     load_order: i64,
+    label: String,
+    version: String,
+    category: u64,
+    updated: u64,
     nexus_id: i64,
-    dir: PathBuf,
-    fav: bool,
-    last_updated: DateTime<Local>
 }
 impl Mod {
-    /// Gets the label of the mod
+    /*/// Gets the label of the mod
     pub fn get_label(&self) -> &String {
         return &self.label;
     }
@@ -61,26 +64,97 @@ impl Mod {
     }
     /// Creates a new Mod
     pub fn new() -> Mod {
-    Mod {
-        label: String::from(""),
-        load_order: -1,
-        nexus_id: -1,
-        dir: PathBuf::from(""),
-        fav: false,
-        last_updated: Local::now()
+        Mod {
+            label: String::from(""),
+            load_order: -1,
+            nexus_id: -1,
+            dir: PathBuf::from(""),
+            fav: false,
+            last_updated: Local::now()
+        }
+    }*/
+    pub fn from(list: &ListStore) -> Option<Vec<Mod>> {
+        match list.get_iter_first() {
+            Some(v) => {
+                let mut result: Vec<Mod> = Vec::new();
+                result.push(Mod {
+                    enabled: list
+                        .get_value(&v, 0)
+                        .get::<bool>()
+                        .expect("Cannot get value enabled"),
+                    load_order: list
+                        .get_value(&v, 1)
+                        .get::<i64>()
+                        .expect("Cannot get value load_order"),
+                    label: list
+                        .get_value(&v, 2)
+                        .get::<String>()
+                        .expect("Cannot get value label"),
+                    version: list
+                        .get_value(&v, 3)
+                        .get::<String>()
+                        .expect("Cannot get value version"),
+                    category: list
+                        .get_value(&v, 4)
+                        .get::<u64>()
+                        .expect("Cannot get value category"),
+                    updated: list
+                        .get_value(&v, 5)
+                        .get::<u64>()
+                        .expect("Cannot get value updated"),
+                    nexus_id: list
+                        .get_value(&v, 6)
+                        .get::<i64>()
+                        .expect("Cannot get value nexus_id"),
+                });
+                while list.iter_next(&v) == true {
+                    result.push(Mod {
+                        enabled: list
+                            .get_value(&v, 0)
+                            .get::<bool>()
+                            .expect("Cannot get value enabled"),
+                        load_order: list
+                            .get_value(&v, 1)
+                            .get::<i64>()
+                            .expect("Cannot get value load_order"),
+                        label: list
+                            .get_value(&v, 2)
+                            .get::<String>()
+                            .expect("Cannot get value label"),
+                        version: list
+                            .get_value(&v, 3)
+                            .get::<String>()
+                            .expect("Cannot get value version"),
+                        category: list
+                            .get_value(&v, 4)
+                            .get::<u64>()
+                            .expect("Cannot get value category"),
+                        updated: list
+                            .get_value(&v, 5)
+                            .get::<u64>()
+                            .expect("Cannot get value updated"),
+                        nexus_id: list
+                            .get_value(&v, 6)
+                            .get::<i64>()
+                            .expect("Cannot get value nexus_id"),
+                    });
+                }
+                return Some(result);
+            }
+            None => None,
+        }
+        //list.get_value(list.get_iter_first().unwrap(), 0).get::<String>().unwrap();
     }
-}
 }
 impl std::fmt::Display for Mod {
     fn fmt(&self, _: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        println!("{}", format!("{}{}", "#", &self.load_order));
-        println!("{}", format!("{}{}", "Label: ", &self.label));
-        println!("{}", format!("{}{}", "Nexus: ", &self.nexus_id));
-        match &self.dir.to_str(){
-            Some(v) => println!("{}", format!("{}{}", "Dir: ", v)),
-            None => {}
-        }
-        println!("{}", format!("{}{}", "Last Updated: ", &self.last_updated));
+        println!("{}", format!("{}{}", "~", self.enabled));
+        println!("{}", format!("{}{}", "#", self.load_order));
+        println!("{}", format!("{}{}", "Label: ", self.label));
+        println!("{}", format!("{}{}", "Version: ", self.version));
+        println!("{}", format!("{}{}", "Category: ", self.category));
+        println!("{}", format!("{}{}", "Updated: ", self.updated));
+        println!("{}", format!("{}{}", "Nexus: ", self.nexus_id));
         Ok(())
     }
 }
--- src/moui.rs
+++ src/moui.rs
@@ -2,45 +2,14 @@ use gio;
 use gtk;
 use gtk::prelude::*;
 use gio::prelude::*;
-use gtk::{ApplicationWindow, Builder, Button, TreeStore, Window, WindowType};
-use moenv::Environment;
+use gtk::{ApplicationWindow, Builder, Button, ListStore, TreeStore, Window, WindowType};
+//use moenv::Environment;
 pub struct UI {
     window: gtk::Window,
-    pub env: Environment,
+    //pub env: Environment,
     store_mod_categories: TreeStore,
 }
 impl UI {
-    /// Creates a new UI instance
-    /*pub fn new() -> UI {
-        if gtk::init().is_err() {
-            println!("Failed to initialize gtk");
-            panic!();
-        }
-
-        let glade_src = include_str!("window.glade");
-        let builder = gtk::Builder::new_from_string(glade_src);
-        let window: gtk::Window = builder.get_object("mowindow").unwrap();
-        let mod_categories: TreeStore = builder.get_object::<TreeStore>("treestore-mod-categories").expect("Couldn't get treestore-mod-categories");
-        mod_categories.insert_with_values(None, None, &[0], &[&"aaaa"]);
-        window.set_title("Mod Organizer");
-        window.connect_delete_event(|_, _| {
-            gtk::main_quit();
-            Inhibit(false)
-        });
-        UI {
-            window: window,
-            env: Environment::new(),
-            store_mod_categories: builder.get_object::<TreeStore>("treestore-mod-categories").unwrap()
-        }
-    }*/
-    /// Displays the window
-    pub fn show(&self) -> () {
-        self.window.show_all();
-    }
-    /// Hides the window
-    pub fn hide(&self) -> () {
-        self.window.hide();
-    }
     pub fn build_ui(application: &gtk::Application) {
         let glade_src = include_str!("window.glade");
         let builder = Builder::new_from_string(glade_src);
@@ -49,10 +18,13 @@ impl UI {
             .get_object("mowindow")
             .expect("Couldn't get mowindow");
         window.set_application(application);
+        window.set_title("mofl - Skyrim SE");
         window.connect_delete_event(move |win, _| {
             win.destroy();
             Inhibit(false)
         });
         window.show_all();
+        let mod_vec = super::momod::Mod::from(&builder.get_object::<ListStore>("treestore-mod-list").expect("Cannot load object")).expect("from return failed");
+        println!("{}", mod_vec.get(0).unwrap());
     }
 }
