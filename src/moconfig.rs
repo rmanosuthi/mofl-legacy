922eabf61013f54703ba7b7ea9541cef9704fffc
Ratchaphon Manosuthi <themelo64@gmail.com>
2018-12-29 18:24:36 +0700

    mofl config.json

--- src/moconfig.rs
+++ src/moconfig.rs
@@ -1,4 +1,7 @@
 use std::path::PathBuf;
+use std::env;
+use std::fs;
+use moui::DEFAULT_PATH;
 extern crate serde;
 extern crate serde_json;
 
@@ -6,4 +9,18 @@ extern crate serde_json;
 pub struct Config {
     active_game: PathBuf,
     mofl_version: String
+}
+
+impl Config {
+    pub fn new() -> Config {
+        Config {
+            active_game: PathBuf::new(),
+            mofl_version: env!("CARGO_PKG_VERSION").to_string()
+        }
+    }
+    pub fn init_folders() {
+        let mut tmp_path: PathBuf = PathBuf::from(env::var_os("HOME").unwrap());
+        tmp_path.push(DEFAULT_PATH);
+        fs::create_dir_all(&tmp_path);
+    }
 }
--- src/moui.rs
+++ src/moui.rs
@@ -1,4 +1,6 @@
 use std::path::PathBuf;
+use std::fs;
+use std::env;
 use gio;
 use gtk;
 use gtk::prelude::*;
@@ -8,12 +10,13 @@ use momod::Mod;
 use mogame::Game;
 use moconfig::Config;
 //use moenv::Environment;
-static DEFAULT_PATH: &str = "~/.config/mofl";
+pub static DEFAULT_PATH: &str = ".config/mofl";
 pub struct UI {
     game: Game
 }
 impl UI {
     pub fn build_ui(application: &gtk::Application) {
+        Config::init_folders();
         let glade_src = include_str!("window.glade");
         let builder = Builder::new_from_string(glade_src);
 
@@ -21,15 +24,27 @@ impl UI {
             .get_object("mowindow")
             .expect("Couldn't get mowindow");
         window.set_application(application);
-        window.set_title("mofl - Skyrim SE");
+        window.set_title("mofl");
         window.connect_delete_event(move |win, _| {
             win.destroy();
             Inhibit(false)
         });
         window.show_all();
-        let mut tmp_path: PathBuf = PathBuf::from(DEFAULT_PATH);
+        let mut tmp_path: PathBuf = PathBuf::from(env::var_os("HOME").unwrap());
+        tmp_path.push(DEFAULT_PATH);
         tmp_path.push("config.json");
-        let config: Config = serde_json::from_str(&std::fs::read_to_string(&tmp_path.as_path()).unwrap()).unwrap();
+        let config: Config = UI::read_mofl_config(&tmp_path);
+        println!("{:?}", &config);
+        /*
+        match  {
+            Ok(v) => {
+                serde_json::from_str(&std::fs::read_to_string(&tmp_path.as_path()).unwrap())
+            },
+            Err(e) => {
+                return Config::new();
+            }
+        };
+        */
         /*let mod_vec = super::momod::Mod::from(&builder.get_object::<ListStore>("treestore-mod-list").expect("Cannot load object")).expect("from return failed");
         //println!("{}", mod_vec.get(0).unwrap());
 
@@ -45,5 +60,16 @@ impl UI {
             m.to(list);
         }
         println!("{}", serde_json::to_string(&super::momod::Mod::from(list).unwrap()).unwrap());*/
+    }
+    fn read_mofl_config(tmp_path: &PathBuf) -> Config {
+        match fs::read_to_string(tmp_path.as_path()) {
+            Ok(v) => serde_json::from_str(&v).unwrap(),
+            Err(e) => {
+                println!("Creating new config at {}", tmp_path.display());
+                let new_config = Config::new();
+                fs::write(tmp_path.as_path(), serde_json::to_string(&new_config).unwrap()).unwrap();
+                new_config
+            }
+        }
     }
 }
