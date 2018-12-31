e43d39db6fd0cc03d4e0c65dc24a101426da11a4
Ratchaphon Manosuthi <themelo64@gmail.com>
2018-12-31 11:19:52 +0700

    Exe menu

--- src/moui.rs
+++ src/moui.rs
@@ -4,7 +4,7 @@ use gtk;
 use gtk::prelude::*;
 use gtk::MenuItemExt;
 use gtk::{
-    ApplicationWindow, Builder, Button, Dialog, ListStore, MenuItem, TreeStore, Window, WindowType,
+    ApplicationWindow, Builder, Button, Dialog, ListStore, Menu, MenuItem, TreeStore, Window, WindowType,
 };
 use moconfig::Config;
 use mogame::Game;
@@ -39,7 +39,16 @@ impl UI {
             main_window: Rc::new(builder.get_object("mowindow").unwrap())
         }
     }
+    pub fn register_events(&self) {
+        let pref_window = self.builder.get_object::<Dialog>("window-preferences").unwrap();
+        let window_preferences_bt_close = self.builder.get_object::<Button>("window-preferences-bt-close").unwrap();
+        window_preferences_bt_close.connect_clicked(move |_| {
+            println!("Closing preferences");
+            pref_window.emit_close();
+        });
+    }
     pub fn build_ui(&self, application: &gtk::Application) {
+        self.register_events();
         &self.main_window.set_application(application);
         &self.main_window.set_title("mofl");
         &self.main_window.connect_delete_event(move |win, _| {
@@ -76,6 +85,14 @@ impl UI {
         println!("{:?}", game);
         config.to(&mut exe_list);
         UI::save_game_config(&config, &game);
+        let menu_exe_list = self.builder.get_object::<Menu>("menu-exe-list").unwrap();
+        for ref i in &self.game.executables {
+            let new_item = MenuItem::new_with_label(i.to_str().unwrap());
+            println!("{:?}", &new_item);
+            menu_exe_list.append(&new_item);
+        }
+        &menu_exe_list.show_all(); // IMPORTANT!
+        println!("{:?}", &menu_exe_list);
     }
     fn read_mofl_config(tmp_path: &PathBuf) -> Config {
         match fs::read_to_string(tmp_path.as_path()) {
