8a29d2acbbfe64374cfff120eaf0ef37bea4128c
Ratchaphon Manosuthi <themelo64@gmail.com>
2019-04-08 19:48:55 -0400

    uihelper: text input

--- src/uihelper.rs
+++ src/uihelper.rs
@@ -1,23 +1,24 @@
+use crate::mogame::Game;
+use crate::steam::Steam;
 use gio;
 use gio::prelude::*;
 use gtk;
 use gtk::prelude::*;
 use gtk::{
-    Application, ApplicationWindow, ButtonsType, DialogFlags, FileChooserAction, FileChooserDialog, ListStore,
-    MessageDialog, MessageType, ResponseType, Window,
+    Application, ApplicationWindow, ButtonsType, DialogFlags, FileChooserAction, FileChooserDialog,
+    ListStore, MessageDialog, MessageType, ResponseType, Window,
 };
+use std::cell::RefCell;
 use std::error::Error;
 use std::path::Path;
 use std::path::PathBuf;
 use std::rc::Rc;
-use crate::mogame::Game;
-use crate::steam::Steam;
 
 pub struct UIHelper {}
 impl UIHelper {
     pub fn prompt_new_game(steam: Rc<Steam>, list_store: Rc<ListStore>) -> Game {
         // TODO - Actually return a proper Game
-        return Game::new("".to_string(), steam, None, list_store);
+        return Game::new("".to_string(), "".to_string(), steam, None, list_store);
     }
     pub fn serde_err(path: &Path, err: &serde_json::error::Error) {
         let err_message_1 = format!("(De)serialization error from file {:?}", &path);
@@ -32,6 +33,23 @@ impl UIHelper {
         error!("{}: {}", &err_message_1, &err_message_2);
         err_dialog.run();
         panic!();
+    }
+    pub fn dialog_text_input(title: &str, message: &str) -> String {
+        let mut result = Rc::new(RefCell::new(String::new()));
+        let dialog = gtk::MessageDialog::new::<MessageDialog>(
+            None,
+            DialogFlags::MODAL,
+            MessageType::Question,
+            ButtonsType::OkCancel,
+            message,
+        );
+        let text_entry = gtk::Entry::new();
+        let mut r = result.clone();
+        text_entry.connect_activate(move |s| {
+            r.borrow_mut().push_str(&s.get_text().unwrap());
+        });
+        dialog.run();
+        return result.clone().borrow().clone();
     }
     pub fn dialog_path_crit(title: &str, on_err: Option<&str>) -> PathBuf {
         let dialog_choose_mod = FileChooserDialog::with_buttons::<Window>(
@@ -104,5 +122,11 @@ impl UIHelper {
     // This should never be called anyway
     pub fn blank_app_window() -> Rc<ApplicationWindow> {
         panic!("blank_app_window() called (serde tried to deserialize a skipped field)");
+    }
+    pub fn serde_dialog_text_input() -> String {
+        return UIHelper::dialog_text_input(
+                                          "Please provide the game's Steam name",
+                                          &format!("Active game declared but cannot find configuration.\nThe game's Steam name is needed to proceed.")
+                                      );
     }
 }
